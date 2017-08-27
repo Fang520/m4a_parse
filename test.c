64f2b75c94ef04589a266dcb682180a64e89036d
erwang <eric.y.wang@nokia.com>
2017-08-27 11:31:25 -0700

    fix freq_index

--- test.c
+++ test.c
@@ -20,8 +20,11 @@ typedef void (*sample_cb_t)(int offset, int len, FILE* fp);
 char filename[64];
 char m4a_buf[BUF_LEN];
 int m4a_len;
-int sample_rate;
+int sample_freq_index;
 int sample_channel;
+int sample_object_type;
+unsigned char asc_buf[64]; // AudioSpecificConfig
+int asc_len;
 
 stsc_t stsc_tab[MAX_STSC_NUM];
 int stsc_num;
@@ -29,57 +32,16 @@ int chunk_offset_tab[MAX_CHUNK_NUM];
 int chunk_num;
 int sample_size_tab[MAX_SAMPLE_NUM];
 int sample_num;
-int mdat_len;
 
 void get_adts_head(char head[7], int len)
 {
-    int freqs[] = {96000,88200,64000,48000,44100,32000,24000,22050,16000,12000,11025,8000,7350};
-    int i;
-    
-    for (i=0; i<sizeof(freqs)/sizeof(int); i++)
-    {
-        if (freqs[i] == sample_rate)
-        {
-            break;
-        }
-    }
-    
     head[0]  = 0xff;
     head[1]  = 0xf1;
-    head[2]  = 0x40 | (unsigned char)(i<<2);
+    head[2]  = (unsigned char)((sample_object_type-1)<<6) | (unsigned char)(sample_freq_index<<2);
     head[3]  = (unsigned char)(sample_channel<<6) | (unsigned char)(((len+7)>>11)&0x3); 
-    //head[3]  = (unsigned char)(1<<6) | (unsigned char)(((len+7)>>11)&0x3); 
     head[4]  = (unsigned char)(((len+7)>>3)&0xff);
     head[5]  = 0x1f | (unsigned char)(((len+7)<<5)&0xe0);
     head[6]  = 0xfc;
-}
-
-void parse_mdat()
-{
-    char* pos = memmem(m4a_buf, m4a_len, "mdat", 4);
-    if (pos)
-    {
-        mdat_len = htonl(*(int*)(pos - 4)) - 8;
-        printf("mdat len: %d\n", mdat_len);
-    }
-}
-
-void parse_stsd()
-{
-    char* pos = memmem(m4a_buf, m4a_len, "stsd", 4);
-    if (pos)
-    {
-        pos += 36; //channel
-        sample_channel = htons(*(short*)pos);
-        printf("channel: %d\n", sample_channel);
-        pos += 2; //sample size
-        printf("sample size: %d\n", htons(*(short*)pos));
-        pos += 2; //pre define
-        printf("pre define: %d\n", htons(*(short*)pos));
-        pos += 4; //sampel rate
-        sample_rate = htonl(*(unsigned int*)pos) >> 16;
-        printf("sample rate: %d\n", sample_rate);
-    } 
 }
 
 void parse_stsc()
@@ -128,14 +90,72 @@ void check_m4a_file()
         pos += 16;
         if (memcmp(pos, "mp4a", 4) != 0)
         {
-            printf("Only support m4a file, not found mp4a type\n");
+            printf("only support m4a file, not found mp4a type\n");
             exit(1);
         }
     }
     else
     {
-        printf("Only support m4a file, not found stsd box\n");
+        printf("only support m4a file, not found stsd box\n");
         exit(1);
+    }
+}
+
+// this field is variable length, param vl return the real length 
+int calc_desc_len(char* pos, int* vl)
+{
+    int len = 0;
+    int i;
+    for (i=0; i<4; i++)
+    { 
+        int c = pos[i];
+        len = (len << 7) | (c & 0x7f);
+        if (!(c & 0x80))
+            break;
+    }
+    *vl = i + 1;
+    return len;    
+}
+
+void parse_esds()
+{
+    char* pos = memmem(m4a_buf, m4a_len, "esds", 4);
+    if (pos)
+    {
+        int len, vl;
+        pos += 8; // MP4ESDescrTag
+        if (*pos != 0x03)
+        {
+            printf("not found MP4ESDescrTag\n");
+            exit(1);
+        }
+        pos++;
+        len = calc_desc_len(pos, &vl);
+        pos += (vl + 3); // MP4DecConfigDescrTag
+        if (*pos != 0x04)
+        {   
+            printf("not found MP4DecConfigDescrTag\n");
+            exit(1);
+        }
+        pos++;
+        len = calc_desc_len(pos, &vl);
+        pos += (vl + 13); // MP4DecSpecificDescrTag
+        if (*pos != 0x05)
+        {
+            printf("not found MP4DecSpecificDescrTag\n");
+            exit(1);
+        }
+        pos++;
+        len = calc_desc_len(pos, &vl);
+        pos += vl;
+        memcpy(asc_buf, pos, len);
+        asc_len = len;
+        sample_object_type = (asc_buf[0] & 0xf8) >> 3;
+        sample_freq_index = ((asc_buf[0] & 0x7) << 1) | (asc_buf[1] >> 7);
+        sample_channel = (asc_buf[1] >> 3) & 0x0f;
+        printf("sample_object_type: %d\n", sample_object_type);
+        printf("sample_freq_index: %d\n", sample_freq_index);
+        printf("sample_channel: %d\n", sample_channel);
     }
 }
 
@@ -228,10 +248,6 @@ void copy_raw_to_es()
     printf("save es successful\n");
 }
 
-void copy_raw_to_latm()
-{
-}
-
 void adts_cb(int offset, int len, FILE* fp)
 {
     char head[7];
@@ -272,15 +288,11 @@ int main(int argc, char** argv)
     
     load_m4a_file(argv[1]);
     check_m4a_file();
-    parse_mdat();
-    parse_stsd();
+    parse_esds();
     parse_stsc();
     parse_stsz();
     parse_stco();
     copy_raw_to_es();
-    copy_raw_to_latm();
     copy_raw_to_adts();
-    
     return 0;
 }    
-
